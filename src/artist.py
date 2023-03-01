e073f96116abf0281a716cc64355f06cca5f2f14
Duane <duanerichard47@hotmail.com>
2023-03-01 00:10:01 -0500

    cmplted

--- src/artist.py
+++ src/artist.py
@@ -17,3 +17,23 @@ class Artist:
 # testing = Artist("mouse")
 # print(Artist.get_records)
 # print(testing.name)
+
+'''
+class Artist:
+    def __init__(self, name):
+        self.name = name
+        self.records = []
+    
+    def get_name(self):
+        return self.name
+    
+    def get_records(self):
+        return self.records
+    
+    def get_first_record(self):
+        min_record = self.records[0]
+        for record in self.records:
+            if record.year < min_record.year:
+                min_record = record
+        return min_record
+'''
--- src/record.py
+++ src/record.py
@@ -40,6 +40,44 @@ class Record:
                 longest_song = song.runtime
                 longest_song_index = x
             return self.songs[longest_song_index]
+        
+        """
+        class Record:
+    def __init__(self, title, artist, year):
+        self.title = title.title()
+        self.artist = artist
+        self.year = year
+        self.songs = []
+    
+    def get_title(self):
+        return self.title
+    
+    def get_artist(self):
+        return self.artist
+    
+    def get_year(self):
+        return self.year
+    
+    def get_songs(self):
+        return self.songs
+    
+    def total_runtime(self):
+        # total_run = 0
+        # for song in self.songs:
+        #     total_run += song.runtime
+        # return total_run
+        return sum([song.runtime for song in self.songs])
+
+    def has_song(self, song):
+        return song in self.songs
+    
+    def get_longest_song(self):
+        longest = self.songs[0]
+        for song in self.songs:
+            if song.runtime > longest.runtime:
+                longest = song
+        return longest
+        """
 
     
         
